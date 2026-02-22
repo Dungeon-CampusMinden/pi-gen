ee507672e0ba11c4dfd6228c1f6a9866a14aa099
Jan Hokamp <jan.hokamp@gmail.com>
2026-02-22 16:06:07 +0100

    Ensure wifi settings

--- stage6-dungeon/01-setup-wifi/00-run.sh
+++ stage6-dungeon/01-setup-wifi/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 if [ -v WPA_ESSID ] && [ -v WPA_PASSWORD ]; then
+	mkdir -p "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
 	install -m 600 files/default-wifi.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/"
 	sed -i "s/PLACEHOLDER_SSID/${WPA_ESSID}/" "${ROOTFS_DIR}/etc/NetworkManager/system-connections/default-wifi.nmconnection"
 	sed -i "s/PLACEHOLDER_PASSWORD/${WPA_PASSWORD}/" "${ROOTFS_DIR}/etc/NetworkManager/system-connections/default-wifi.nmconnection"
