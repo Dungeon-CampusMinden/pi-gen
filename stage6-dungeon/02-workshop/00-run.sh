b6410d4943dcbb4c14b7c13f35fed457a19abdf5
Jan Hokamp <jan.hokamp@gmail.com>
2026-02-23 17:41:32 +0100

    Add basic workshop setup
    Setup folder on Desktop and checkout lastest github repo

---
+++ stage6-dungeon/02-workshop/00-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+on_chroot << EOF
+install -m 755 -o 1000 -g 1000 -d "/home/${FIRST_USER_NAME}/Desktop/"
+install -m 755 -o 1000 -g 1000 -d "/home/${FIRST_USER_NAME}/Desktop/Workshop/"
+
+runuser -l ${FIRST_USER_NAME} -c 'git -C /home/${FIRST_USER_NAME}/Desktop/Workshop/source pull || git clone --depth 1 https://github.com/Dungeon-CampusMinden/Dungeon.git /home/${FIRST_USER_NAME}/Desktop/Workshop/source'
+EOF
