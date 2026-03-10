a6bc9aa9f934d43661613d669c8da16fabe2c9b4
Jan Hokamp <jan.hokamp@gmail.com>
2026-03-10 17:18:59 +0100

    Add blockly jar
    Download latest jar for blockly and create desktop entry

--- stage6-dungeon/04-blockly/00-run.sh
+++ stage6-dungeon/04-blockly/00-run.sh
@@ -1,23 +1,17 @@
 #!/bin/bash -e
 
 install -m 755 -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/Workshop/Blockly/"
-install -m 755 -o 1000 -g 1000 files/Blockly_server.bin "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/Workshop/Blockly/"
 
-install -m 755 -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/Workshop/Blockly/content/"
-install -m 644 -o 1000 -g 1000 files/blockly.jar "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/Workshop/Blockly/content/"
-install -m 644 -o 1000 -g 1000 files/index.html "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/Workshop/Blockly/content/"
+curl -L -o "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/Workshop/Blockly/Blockly-web.jar" "https://github.com/Dungeon-CampusMinden/Dungeon/releases/latest/download/Blockly-web.jar"
+chown 1000:1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/Workshop/Blockly/Blockly-web.jar"
 
-cat > "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/blockly.desktop" <<- EOF
+cat > "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/blockly-dungeon.desktop" <<- EOF
 [Desktop Entry]
 Name=Blockly Dungeon
 Type=Application
 Icon=/home/${FIRST_USER_NAME}/Desktop/Workshop/source/blockly/frontend/public/cat_logo.png
-Exec=/home/${FIRST_USER_NAME}/Desktop/Workshop/Blockly/Blockly_server.bin
+Exec=sh -c "java -jar /home/${FIRST_USER_NAME}/Desktop/Workshop/Blockly/Blockly-web.jar & xdg-open http://localhost:8081"
 Path=/home/${FIRST_USER_NAME}/Desktop/Workshop/Blockly/
 EOF
-chown 1000:1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/blockly.desktop"
-chmod 664 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/blockly.desktop"
-
-on_chroot << EOF
-cp /home/${FIRST_USER_NAME}/Desktop/Workshop/source/blockly/frontend/public/* /home/${FIRST_USER_NAME}/Desktop/Workshop/Blockly/content/
-EOF
+chown 1000:1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/blockly-dungeon.desktop"
+chmod 664 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/blockly-dungeon.desktop"
