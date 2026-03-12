8bf44fe5d2e4262dea9223c26b969857e836c220
Jan Hokamp <jan.hokamp@gmail.com>
2026-03-12 14:09:51 +0100

    Seperate Java and Advanced Dungeon VS Code installs

--- stage6-dungeon/03-tools/00-run.sh
+++ stage6-dungeon/03-tools/00-run.sh
@@ -3,5 +3,21 @@
 install -m 664 -o 1000 -g 1000 files/code.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/"
 
 on_chroot << EOF
-runuser -l ${FIRST_USER_NAME} -c 'DONT_PROMPT_WSL_INSTALL=1 code --install-extension /home/${FIRST_USER_NAME}/Desktop/Workshop/source/doc/produs_unterlagen/blockly-code-runner-1.0.2.vsix'
+
+install -m 755 -o 1000 -g 1000 -d "/home/${FIRST_USER_NAME}/Desktop/Workshop/.code/Blockly/data/"
+install -m 755 -o 1000 -g 1000 -d "/home/${FIRST_USER_NAME}/Desktop/Workshop/.code/Blockly/extensions/"
+
+runuser -l ${FIRST_USER_NAME} -c 'DONT_PROMPT_WSL_INSTALL=1 code \
+  --user-data-dir /home/${FIRST_USER_NAME}/Desktop/Workshop/.code/Blockly/data \
+  --extensions-dir /home/${FIRST_USER_NAME}/Desktop/Workshop/.code/Blockly/extensions \
+  --install-extension /home/${FIRST_USER_NAME}/Desktop/Workshop/source/doc/produs_unterlagen/blockly-code-runner-1.0.2.vsix'
+
+install -m 755 -o 1000 -g 1000 -d "/home/${FIRST_USER_NAME}/Desktop/Workshop/.code/Java/data/"
+install -m 755 -o 1000 -g 1000 -d "/home/${FIRST_USER_NAME}/Desktop/Workshop/.code/Java/extensions/"
+
+runuser -l ${FIRST_USER_NAME} -c 'DONT_PROMPT_WSL_INSTALL=1 code \
+  --user-data-dir /home/${FIRST_USER_NAME}/Desktop/Workshop/.code/Java/data \
+  --extensions-dir /home/${FIRST_USER_NAME}/Desktop/Workshop/.code/Java/extensions \
+  --install-extension vscjava.vscode-gradle \
+  --install-extension redhat.java'
 EOF
