33a0bd1d3adf06c282eb508204cf059344f7c0c7
Jan Hokamp <jan.hokamp@gmail.com>
2026-03-13 08:50:46 +0100

    Preconfigure and compile gradle project

--- stage6-dungeon/06-advanced/00-run.sh
+++ stage6-dungeon/06-advanced/00-run.sh
@@ -9,3 +9,8 @@ Exec=code --disable-workspace-trust --user-data-dir /home/${FIRST_USER_NAME}/Des
 EOF
 chown 1000:1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/advanced-dungeon.desktop"
 chmod 664 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/advanced-dungeon.desktop"
+
+on_chroot << EOF
+# Have to kill java processes to make sure that the gradle daemon is not running, otherwise can not unmount filesystems
+runuser -l ${FIRST_USER_NAME} -c '(cd ~/Desktop/Workshop/source && ./gradlew --no-daemon --console=plain assemble) && killall java'
+EOF
